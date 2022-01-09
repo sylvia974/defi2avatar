cade92432abad1c2891364530b0180139d447782
paquiry <paquirysylvia974@gmail.com>
2022-01-09 20:30:28 +0400

    modification du projet flutter avec les modifications de la font Source Sans Pro, font weight et la couleur

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -10,46 +12,51 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.teal,
-    body: SafeArea(
-      child:
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-
-            Center(
-              child:
-              Container(
-                width: 200,
-                height: 200,
-                child: CircleAvatar(
-                  backgroundImage: AssetImage("images/de.png"),
+      home: Scaffold(
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: Container(
+                    width: 200,
+                    height: 200,
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage("images/de.png"),
+                    ),
+                  ),
                 ),
-
-              ),
-            ),
-            Text('PAQUIRY Sylvia',
-                style: TextStyle(fontFamily: 'Pacifico',
+                Text(
+                  'PAQUIRY Sylvia',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
                     color: Colors.teal[100],
                     letterSpacing: 2.5,
                     fontWeight: FontWeight.bold,
-
-
+                  ),
                 ),
+                Text(
+                  'Bonjour',
+                  style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    color: Colors.teal[100],
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
             ),
-            Text('Bonjour',
-              style: TextStyle(fontFamily: 'SourceSansPro',
-                color: Colors.teal[100],
-                letterSpacing: 2.5,
-                fontWeight: FontWeight.bold,
-            ),
-            ),
-          ],
-        ),
-      )
-  ),
-    )
-    ;
+          )),
+    );
   }
 }
+
+
+
+
+
+
+
+
+
